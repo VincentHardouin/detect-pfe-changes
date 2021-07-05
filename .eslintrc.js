262cc078cc1bad632cbff42427a1fc38aa7f8ceb
Vincent Hardouin <vincenthardouin@icloud.com>
2021-07-05 19:04:21 +0200

    Add lint configs

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8,
+  },
+  extends: ['plugin:prettier/recommended'],
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+};
